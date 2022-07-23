5836382ba02808c4dbbfb32be8f77fbb5c4c75a6
Imalsha Ranepura <61657083+Imalsha97@users.noreply.github.com>
2022-07-23 16:21:47 -0700

    configure heafer

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import React from 'react';
 import SideMenu from '../components/SideMenu';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  appMain : {
+    paddingLeft:'320px',
+    width:'100%'
+  }
+})
 
 function App() {
+  const classes = useStyles();
   return (
     <>
     <SideMenu/>
+    <div className={classes.appMain}>hhhhhhhhhhhh</div>
     </>
   );
 }
